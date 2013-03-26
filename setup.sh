5d5050ad3a46362ff258b5a2db13c3160abcb783
james-maloney <jamessagan@gmail.com>
2013-03-26 12:49:48 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 git submodule init && git submodule update
-ln -s $HOME/vim_files/.vimrc $HOME/.vimrc
-ln -s $HOME/vim_files/.vim $HOME/.vim
+ln -s $HOME/dotfiles/vim/vim_files/.vimrc $HOME/.vimrc
+ln -s $HOME/dotfiles/vim/vim_files/.vim $HOME/.vim
 cd $HOME/.vim/bundle/command-t/ruby/command-t
 ruby extconf.rb
 make
